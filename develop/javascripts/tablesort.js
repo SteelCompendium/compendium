2b3ee49842a34ce98b9dace9bef35e724fdd6b32
Scott Tomaszewski <scottTomaszewski@gmail.com>
2026-02-01 10:17:27 -0500

    Removing develop

--- develop/javascripts/tablesort.js
+++
@@ -1,5 +0,0 @@
-document$.subscribe(function () {
-  const selector = "article table:not([class]):has(tbody > tr + tr)";
-  const tables = document.querySelectorAll(selector);
-  tables.forEach((table) => new Tablesort(table));
-});
